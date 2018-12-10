4b43ad256509c33c1dcc7cee69f88b400b4d43d6
Matthew Wrobel <mattwrobel@gmail.com>
2018-12-09 21:29:07 -0600

    Converted '_' and '.' characters in part name to '-' for the ECM config files.

--- ecm_engines_cfg_generator.py
+++ ecm_engines_cfg_generator.py
@@ -23,7 +23,7 @@ def generate_ecm_engines(parts):
     for part in parts:
         if part['name'] is not None and len(part['name']) > 0:
             if part['mod'] == 'Engine_Config' and not part['orphan']:
-                ecm_configs += module_part_config_template.substitute(name=part['name'], ecm=part['entry_cost_mods'])
+                ecm_configs += module_part_config_template.substitute(name=part['name'].replace('_','-').replace('.','-'), ecm=part['entry_cost_mods'])
     text_file = open("output/ECM-Engines.cfg", "w")
     text_file.write(tree_ecm_engines_header)
     text_file.write(ecm_configs)
